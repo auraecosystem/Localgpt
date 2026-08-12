ca9b1f33cb71ccf25d55de3c45705d4cfdacb263
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-12 12:35:21 +0100

    Create run.sh

---
+++ localgpt-gen/world.glb/run.sh
@@ -0,0 +1,11 @@
+# Start interactive mode
+localgpt-gen
+
+# Start with an initial prompt
+localgpt-gen "Create a desert scene with pyramids and a UFO hovering above"
+
+# Load an existing scene
+localgpt-gen --scene ./world.glb
+
+# Verbose logging
+localgpt-gen --verbose
